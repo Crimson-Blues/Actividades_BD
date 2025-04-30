4072588cee0efefb6cc159ff4bed12e79500be99
Juan Diego Cardenas Mejia <juan.diego.cardenas@correounivalle.edu.co>
2025-04-29 22:25:28 -0500

    Create Data-Definition.sql

---
+++ GastroDB/Data-Definition.sql
@@ -0,0 +1,60 @@
+----DATA DEFINITION-----
+
+--Tabla cliente
+--Representa las personas que ordenan en el restaurante
+CREATE TABLE cliente(
+id_cliente INT PRIMARY KEY,
+nombre VARCHAR(45),
+apellido VARCHAR(45),
+observaciones VARCHAR(45)
+);
+
+--Tabla mesero
+--Representa los trabajadores que reciben las ordenes de los comensales
+CREATE TABLE mesero(
+id_mesero INT PRIMARY KEY,
+nombre VARCHAR(45) NOT NULL,
+apellido1 VARCHAR(45) NOT NULL,
+apellido2 VARCHAR(45)
+);
+
+--Tabla platillo
+--Representa los platillos que ofrece el restaurante
+CREATE TABLE platillo(
+id_platillo INT PRIMARY KEY,
+nombre VARCHAR(45),
+importe INT
+);
+
+--Tabla bebida
+--Representa las bebidas ofrecidas
+CREATE TABLE bebida(
+id_bebida INT PRIMARY KEY,
+nombre VARCHAR(45),
+importe INT
+);
+
+--Tabla mesa
+--Representa la mesa dónde se sientan los comensales
+CREATE TABLE mesa(
+id_mesa INT PRIMARY KEY,
+num_Comensales INT,
+ubicacion VARCHAR(45)
+);
+
+--Tabla factura
+--Representa un registro de la orden de platillo y bebida de un cliente sentado en una mesa
+CREATE TABLE factura(
+id_factura INT,
+fecha_factura DATE,
+id_cliente INT NOT NULL,
+id_mesero INT NOT NULL,
+id_mesa INT NOT NULL,
+id_platillo INT NOT NULL,
+id_bebida INT NOT NULL,
+FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente) ON DELETE SET NULL,
+FOREIGN KEY(id_mesero) REFERENCES mesero(id_mesero) ON DELETE SET NULL,
+FOREIGN KEY(id_mesa) REFERENCES mesa(id_mesa) ON DELETE SET NULL,
+FOREIGN KEY(id_platillo) REFERENCES platillo(id_platillo) ON DELETE SET NULL,
+FOREIGN KEY(id_bebida) REFERENCES bebida(id_bebida) ON DELETE SET NULL
+);
